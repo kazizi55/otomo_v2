5dbb2bc924374c8458c06e48be38ced3eecb54cb
kazizi55 <kaz.redhot1201@gmail.com>
2020-04-28 13:08:06 +0900

    band,live,sessionの一覧ページを作成

--- app/javascript/packs/views/band/Index.vue
+++ app/javascript/packs/views/band/Index.vue
@@ -1,5 +1,34 @@
 <template>
   <div>
-    <h1>募集中バンド一覧</h1>
+    <h1>バンドメンバー募集一覧</h1>
+    <table>
+      <tbody>
+        <tr>
+          <th></th>
+          <th>バンド名</th>
+        </tr>
+        <tr v-for="band in bands" :key="band.id">
+          <td>{{ band.id }}</td>
+          <td>{{ band.name }}</td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data(){
+    return {
+      bands: []
+    }
+  },
+  created(){
+    axios
+      .get('/api/v1/bands')
+      .then(response => (this.bands = response.data))
+  }
+}
+</script>
--- app/javascript/packs/views/live/Index.vue
+++ app/javascript/packs/views/live/Index.vue
@@ -1,5 +1,34 @@
 <template>
   <div>
-    <h1>ライブ一覧</h1>
+    <h1>ライブ告知一覧</h1>
+    <table>
+      <tbody>
+        <tr>
+          <th></th>
+          <th>ライブ名</th>
+        </tr>
+        <tr v-for="live in lives" :key="live.id">
+          <td>{{ live.id }}</td>
+          <td>{{ live.name }}</td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data(){
+    return {
+      lives: []
+    }
+  },
+  created(){
+    axios
+      .get('/api/v1/lives')
+      .then(response => (this.lives = response.data))
+  }
+}
+</script>
