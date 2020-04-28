e336b8dfe3c5f02c15a11325d2b02f0e1ea5ce04
kazizi55 <kaz.redhot1201@gmail.com>
2020-04-28 16:09:51 +0900

    band,session,liveに詳細ページを追加

---
+++ app/javascript/packs/views/session/Show.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <p>セッション名：{{ session.name }}</p>
+    <p>説明：{{ session.explanation }}</p>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data(){
+    return {
+      session: {}
+    }
+  },
+  mounted(){
+    axios
+      .get(`/api/v1/sessions/${this.$route.params.id}`)
+      .then(response => (this.session = response.data))
+  }
+}
+</script>
