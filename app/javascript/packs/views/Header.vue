0a4063189a2848887a7fc1f987598bd46e7e9332
kazizi55 <kaz.redhot1201@gmail.com>
2020-04-29 12:17:00 +0900

    フォームにscriptを導入しmaterializeを用いてcss適用

--- app/javascript/packs/views/Header.vue
+++ app/javascript/packs/views/Header.vue
@@ -5,10 +5,38 @@
     <router-link to="/band">バンドメンバー募集</router-link>
     <router-link to="/session">セッションメンバー募集</router-link>
     <router-link to="/live">ライブ告知</router-link>
-    <router-link to="/signin">Sign In</router-link>
-    <router-link to="/signup">Sign Up</router-link>
+    <router-link to="/signin" v-if="!signedIn">Sign In</router-link>
+    <router-link to="/signup" v-if="!signedIn">Sign Up</router-link>
+    <a href="/" v-if="signedIn" @click="signOut">Sign out</a>
   </div>
 </template>
+
+<script>
+  import { mapState } from 'vuex'
+
+  export default {
+    name: 'Header',
+    computed: mapState([
+      'signedIn'
+    ]),
+    mounted: function() {
+      this.$store.dispatch('doFetchSignedIn')
+    },
+    methods: {
+      setError(error, text) {
+        this.error = (error.response && error.response.data && error.response.data.error) || text
+      },
+      signOut() {
+        this.$http.secured.delete(`/api/signin`)
+          .then(response => {
+            delete localStorage.csrf
+            delete localStorage.signedIn
+          })
+          .catch(error => this.setError(error, 'Cannot sign out'))
+      }
+    }
+  }
+</script>
 
 <style scoped>
   .title {
