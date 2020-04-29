f00666bcff52c302645de959712823ffc0753d99
kazizi55 <kaz.redhot1201@gmail.com>
2020-04-29 23:47:28 +0900

    ジャムセッションのフロントエンドを実装

---
+++ app/javascript/packs/views/jam_sessions/Index.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <h1>セッションメンバー募集一覧</h1>
+    <table>
+      <tbody>
+        <tr>
+          <th></th>
+          <th>セッション</th>
+        </tr>
+        <tr v-for="jam in jamSessions" :key="jam.id">
+          <td>{{ jam.id }}</td>
+          <td>{{ jam.name }}</td>
+          <td><router-link :to='{ name: "JamSessionShow", params: { id: jam.id } }'>詳細</router-link></td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data(){
+    return {
+      jamSessions: []
+    }
+  },
+  created(){
+    axios
+      .get('/api/jam_sessions')
+      .then(response => (this.jamSessions = response.data))
+  }
+}
+</script>
