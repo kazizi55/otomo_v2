380c9a8e57769f26fe719785b8bddaeba10f9833
kazizi55 <kaz.redhot1201@gmail.com>
2020-04-29 11:27:14 +0900

    userのmodel,contoller,seedを作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_27_115445) do
+ActiveRecord::Schema.define(version: 2020_04_29_021100) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -43,6 +43,14 @@ ActiveRecord::Schema.define(version: 2020_04_27_115445) do
   create_table "sessions", force: :cascade do |t|
     t.string "name", default: "", null: false
     t.text "explanation", default: "", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "email", null: false
+    t.string "password_digest", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
